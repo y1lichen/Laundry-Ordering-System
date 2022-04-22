a3019df6e3caa4c0d02b70539bf062a709e1bffb
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-04-23 00:39:10 +0800

    update find available reservations

--- src/main/java/com/jefferson/laundryorderingsystem/entities/reservation/ReservationController.java
+++ src/main/java/com/jefferson/laundryorderingsystem/entities/reservation/ReservationController.java
@@ -1,14 +1,12 @@
 package com.jefferson.laundryorderingsystem.entities.reservation;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.ZoneId;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -20,24 +18,55 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping(value = "reservations")
 public class ReservationController {
 
+    // total amount of laundry machine
+    private static int totalMachine = 18;
+
     Logger logger = LoggerFactory.getLogger(ReservationController.class);
 
-    private static String[] possibleTime = { "16:00", "16:40", "17:20", "18:00", "18:40",
-            "19:20", "20:00", "20:40", "21:20", "22:00", "22:40", "23:20" };
+    private static String[] possibleTime = { "16:00:00", "16:40:00", "17:20:00", "18:00:00", "18:40:00",
+            "19:20:00", "20:00:00", "20:40:00", "21:20:00", "22:00:00", "22:40:00", "23:20:00" };
 
-    private static class GetAvaliableReservationsResponse {
-        private int replyCode;
+    private static class GetAvailableReservationsResponse {
+        private int replyCode = -1;
+        private ArrayList<String> availableTimeList = new ArrayList<>();
+
+        public void setReplyCode(int replyCode) {
+            this.replyCode = replyCode;
+        }
+
+        public int getReplyCode() {
+            return replyCode;
+        }
+
+        public void addToAvaliableTimeList(String time) {
+            availableTimeList.add(time);
+        }
+
+        public ArrayList<String> getAvailableTimeList() {
+            return availableTimeList;
+        }
     }
 
     @Autowired
     ReservationRepo repo;
 
     @GetMapping(value = "/get-avaliable-reservations", produces = "application/json")
-    public ResponseEntity<?> getAvaliableReservation(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate time) {
-        LocalDateTime localDateTime = time.atStartOfDay();
-        GetAvaliableReservationsResponse responseBody = new GetAvaliableReservationsResponse();
-        List<Reservation> unavaliableReservations = repo.findAllByTimeAfter(localDateTime);
-        logger.info(unavaliableReservations.toString());
+    public ResponseEntity<?> getAvaliableReservation(@RequestParam String time) {
+        GetAvailableReservationsResponse responseBody = new GetAvailableReservationsResponse();
+        for (int i=0; i<=12; i++) {
+            LocalDateTime localDateTime = LocalDateTime.parse(String.format("%sT%s", time, possibleTime[i]));
+            List<Reservation> reservationsOfSpecificTime = repo.findAllByTime(localDateTime);
+            if (reservationsOfSpecificTime.size() < totalMachine) {
+                // avaliable
+                responseBody.setReplyCode(1);
+                responseBody.addToAvaliableTimeList(reservationsOfSpecificTime.get(0).getTime().toString());
+                return new ResponseEntity<>(responseBody, HttpStatus.OK);
+            } else {
+                // unavaliable
+                responseBody.setReplyCode(-1);
+                return new ResponseEntity<>(responseBody, HttpStatus.ACCEPTED);
+            }
+        }
         return new ResponseEntity<>(responseBody, HttpStatus.BAD_REQUEST);
     }
 
--- src/main/java/com/jefferson/laundryorderingsystem/entities/reservation/ReservationRepo.java
+++ src/main/java/com/jefferson/laundryorderingsystem/entities/reservation/ReservationRepo.java
@@ -1,17 +1,16 @@
 package com.jefferson.laundryorderingsystem.entities.reservation;
 
-import org.springframework.stereotype.Repository;
-
 import java.time.LocalDateTime;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
 
 @Repository
 public interface ReservationRepo extends JpaRepository<Reservation, Integer> {
-    
-    @Query("SELECT r FROM reservations r WHERE r.time >= time")
-    List<Reservation> findAllByTimeAfter(@Param("time") LocalDateTime localDateTime);
+
+    @Query("SELECT r FROM reservations r WHERE r.time = time")
+    List<Reservation> findAllByTime(@Param("time") LocalDateTime localDateTime);
 }
