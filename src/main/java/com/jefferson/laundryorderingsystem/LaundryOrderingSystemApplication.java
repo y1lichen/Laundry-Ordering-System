66a7448ea68fc022060cf2beffc45f3092604760
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-05-16 01:12:09 +0800

    remove scheduling relative stuff

--- src/main/java/com/jefferson/laundryorderingsystem/LaundryOrderingSystemApplication.java
+++ src/main/java/com/jefferson/laundryorderingsystem/LaundryOrderingSystemApplication.java
@@ -1,17 +1,5 @@
 package com.jefferson.laundryorderingsystem;
 
-import static org.quartz.CronScheduleBuilder.cronSchedule;
-import static org.quartz.TriggerBuilder.newTrigger;
-
-import java.util.TimeZone;
-
-import com.jefferson.laundryorderingsystem.utils.CleanDatabaseJob;
-
-import org.quartz.JobBuilder;
-import org.quartz.JobDetail;
-import org.quartz.Scheduler;
-import org.quartz.Trigger;
-import org.quartz.impl.StdSchedulerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -20,12 +8,6 @@ public class LaundryOrderingSystemApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(LaundryOrderingSystemApplication.class, args);
-		// clean up old data
-		Scheduler scheduler = StdSchedulerFactory.getDefaultScheduler();
-		scheduler.start();
-		JobDetail jobDetail = JobBuilder.newJob(CleanDatabaseJob.class).build();
-		Trigger trigger = newTrigger().startNow().withSchedule(cronSchedule("* * 0 * * ?").inTimeZone(TimeZone.getTimeZone("Asia/Taipei"))).build();
-		scheduler.scheduleJob(jobDetail, trigger);
 	}
 
 }
