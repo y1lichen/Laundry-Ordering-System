13c6d02b8a14aa835ebc8f4fea114e442cf924e6
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-05-29 02:08:58 +0800

    using application event to remove expired reservation when start-up

--- src/main/java/com/jefferson/laundryorderingsystem/LaundryOrderingSystemApplication.java
+++ src/main/java/com/jefferson/laundryorderingsystem/LaundryOrderingSystemApplication.java
@@ -1,9 +1,5 @@
 package com.jefferson.laundryorderingsystem;
 
-import javax.annotation.PostConstruct;
-
-import com.jefferson.laundryorderingsystem.utils.DataCleaner;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -12,12 +8,6 @@ public class LaundryOrderingSystemApplication {
 
 	public static void main(String[] args) throws Exception {
 		SpringApplication.run(LaundryOrderingSystemApplication.class, args);
-	}
-
-	@PostConstruct
-	public void init() {
-		DataCleaner dataCleaner = new DataCleaner();
-		dataCleaner.removeExpiredReservation();
 	}
 
 }
--- src/main/java/com/jefferson/laundryorderingsystem/utils/DataCleaner.java
+++ src/main/java/com/jefferson/laundryorderingsystem/utils/DataCleaner.java
@@ -10,13 +10,18 @@ import com.jefferson.laundryorderingsystem.entities.reservation.Reservation;
 import com.jefferson.laundryorderingsystem.entities.reservation.ReservationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
+import org.springframework.stereotype.Component;
 
-public class DataCleaner {
+@Component
+public class DataCleaner implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Autowired
     private ReservationService reservationService;
 
-	public void removeExpiredReservation() {
+	@Override
+	public void onApplicationEvent(ContextRefreshedEvent event) {
 		LocalDateTime today = LocalDateTime.of(LocalDate.now(ZoneId.of("Asia/Taipei")), LocalTime.MIDNIGHT);
 		List<Reservation> expiredReservations = reservationService.getReservationsBeforeSpecificTime(today);
 		for (Reservation reservation : expiredReservations) {
