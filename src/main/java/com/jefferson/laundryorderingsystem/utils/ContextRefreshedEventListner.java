06d8e3f8fd334a9dbd57cdb5d60d2efbc1777a00
Jefferson Chen <f1jefferson@yahoo.com.tw>
2022-05-29 02:23:01 +0800

    change name listener class

--- src/main/java/com/jefferson/laundryorderingsystem/utils/ContextRefreshedEventListner.java
+++ src/main/java/com/jefferson/laundryorderingsystem/utils/ContextRefreshedEventListner.java
@@ -10,18 +10,18 @@ import com.jefferson.laundryorderingsystem.entities.reservation.Reservation;
 import com.jefferson.laundryorderingsystem.entities.reservation.ReservationService;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.context.event.ApplicationReadyEvent;
 import org.springframework.context.ApplicationListener;
+import org.springframework.context.event.ContextRefreshedEvent;
 import org.springframework.stereotype.Component;
 
 @Component
-public class DataCleaner implements ApplicationListener<ApplicationReadyEvent> {
+public class ContextRefreshedEventListner implements ApplicationListener<ContextRefreshedEvent> {
 
 	@Autowired
     private ReservationService reservationService;
 
 	@Override
-	public void onApplicationEvent(ApplicationReadyEvent event) {
+	public void onApplicationEvent(ContextRefreshedEvent event) {
 		LocalDateTime today = LocalDateTime.of(LocalDate.now(ZoneId.of("Asia/Taipei")), LocalTime.MIDNIGHT);
 		List<Reservation> expiredReservations = reservationService.getReservationsBeforeSpecificTime(today);
 		for (Reservation reservation : expiredReservations) {
